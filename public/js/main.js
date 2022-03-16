4e85cbdfb942ee6a581b34d359624a36ece67e72
Hiyaro <nawfal.elkhaznagi@gmail.com>
2022-03-16 14:49:43 +0100

    added the first variable on the main.js file

---
+++ public/js/main.js
@@ -0,0 +1,19 @@
+////////////////////////////////////
+//// code ecrit durant le projet.////
+/////////////////////////////////////
+
+// setup de toutes les variables
+// variable des logos :
+const insider = document.querySelector("#insider");
+const usat = document.querySelector("#usat");
+const theGuardian = document.querySelector("#theguard");
+const techCrunch = document.querySelector("#tcrunch");
+const forbes = document.querySelector("#forbes");
+// variable du bouton qui change la font family
+const fontChange = document.querySelector('.website-font');
+
+// variable pour l'effet remplacement d'un texte par un autre.
+let howItWorks = document.querySelector('.how-it');
+let meals = document.querySelector('.meals');
+let testimonials = document.querySelector('.testimonials');
+let pricing = document.querySelector('.pricing');
